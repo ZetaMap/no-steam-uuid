336dafd758c91ba4b0c26503f43895c8b71b3572
ZetaMap <zack.filmgaming@gmail.com>
2026-07-24 14:57:06 +0200

    updated dependencies, found a safer way to replace Vars.platform

--- src/main/java/fr/zetamap/nosteamuuid/Main.java
+++ src/main/java/fr/zetamap/nosteamuuid/Main.java
@@ -1,9 +1,9 @@
 /*
- * This file is a part of the No Steam UUID plugin for Mindustry.
+ * This file is a part of the No Steam UUID mod for Mindustry.
  *
  * MIT License
  *
- * Copyright (c) 2023-2025 ZetaMap
+ * Copyright (c) 2023-2026 ZetaMap
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,46 +27,70 @@
 package fr.zetamap.nosteamuuid;
 
 import arc.ApplicationListener;
-import arc.files.Fi;
-import arc.struct.Seq;
+import arc.util.Log;
+import arc.util.OS;
 
 import mindustry.ClientLauncher;
 import mindustry.Vars;
+import mindustry.core.Version;
+import mindustry.desktop.DesktopLauncher;
 import mindustry.mod.Mod;
-import mindustry.net.Net;
-import mindustry.type.Publishable;
 
 
 public class Main extends Mod {
-  static { 
-    // Ignore if it's not the steam version
-    if (Vars.steam) {
-      // Assume this is at least a {@link mindustry.ClientLauncher} since only this class set this field.
-      final ClientLauncher original = (ClientLauncher)Vars.platform;
-      
-      Vars.platform = new ClientLauncher() {
-        // Methods from {@link mindustry.ClientLauncher}
-        public void add(ApplicationListener module) { original.add(module); }
-        public void resize(int width, int height) { original.resize(width, height); }
-        public void update() { original.update(); }
-        public void init() { original.init(); }
-        public void resume(){ original.resume(); }
-        public void pause() { original.pause(); }
-        
-        // Methods from {@link mindustry.desktop.DesktopLauncher}
-        public Seq<Fi> getWorkshopContent(Class<? extends Publishable> type) { return original.getWorkshopContent(type); }
-        public void viewListing(Publishable pub) { original.viewListing(pub); }
-        public void viewListingID(String id) { original.viewListingID(id); }
-        public Net.NetProvider getNet() { return original.getNet(); }
-        public void openWorkshop() { original.openWorkshop(); }
-        public void publish(Publishable pub) { original.publish(pub); }
-        public void inviteFriends() { original.inviteFriends(); }
-        public void updateLobby() { original.updateLobby(); }
-        public void updateRPC() { original.updateRPC(); }
-        
-        // Not overridden to keep method from {@link mindustry.core.Platform#getUUID()}
-        //public String getUUID() { return original.getUUID(); }
-      };   
+  public static ClientLauncher original;
+
+  // Init ASAP
+  static { replacePlatform(); }
+
+  public static void replacePlatform() {
+    // Ignore if it's not the steam version or if already initialized
+    if (!Vars.steam || original != null) return;
+    if (!(Vars.platform instanceof ClientLauncher)) {
+      Log.warn("Cannot to hook '@'. Another mod has likely already replaced it.", "Vars.platform");
+      return;
     }
+
+    original = (ClientLauncher)Vars.platform;
+
+    // Suppress values to avoid double desktop initialization
+    System.setProperty("nodiscord", "true");
+    boolean was64Bit = OS.is64Bit; // >b105 only checking for this field
+    OS.is64Bit = false;
+    try { Version.isSteam = false; } // Doesn't exists in >b156
+    catch (NoSuchFieldError ignored) {}
+    boolean wasEnabled = Version.enabled; // >b156 are initializing it in the constructor
+    Version.enabled = false;
+    String lastModifier = Version.modifier;
+    Version.modifier ="";
+
+    Vars.platform = new DesktopLauncher(new String[0]) {
+      // Redirect ApplicationCore methods as it uses a simple array for listeners
+      public void setup() { original.setup(); }
+      public void add(ApplicationListener module) { original.add(module); }
+      public void resize(int width, int height) { original.resize(width, height); }
+      public void update() { original.update(); }
+      public void exit() { original.exit(); }
+      public void init() { original.init(); }
+      public void resume(){ original.resume(); }
+      public void pause() { original.pause(); }
+      // Redirect method because it uses an internal field
+      public void updateRPC() { original.updateRPC(); }
+
+      public String getUUID() {
+        if (!Vars.steam) return super.getUUID();
+        Vars.steam = false; // Disable temporary the steam version. This shouldn't disrupt anything.
+        try { return super.getUUID(); }
+        finally { Vars.steam = true; }
+      }
+    };
+
+    // Set back values
+    System.clearProperty("nodiscord");
+    OS.is64Bit = was64Bit;
+    try { Version.isSteam = true; }
+    catch (NoSuchFieldError ignored) {}
+    Version.enabled = wasEnabled;
+    Version.modifier = lastModifier;
   }
 }
